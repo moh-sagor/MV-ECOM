127fe5d64b2b2754eea01405abdce80b699a0b64
Sagor Hossain <sagorhassain4@gmail.com>
2023-05-14 15:12:55 +0600

    environment setup and role define

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+////////////////////////Admin////////////////
+use App\Http\Controllers\Admin\AdminController;
+
+//////////////////////Vendor////////////////
+use App\Http\Controllers\Vendor\VendorController;
+
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+///////////////////////////////////////////////////
+// Admin Routes//
+///////////////////////////////////////////////////
+
+Route::middleware('auth', 'role:admin')->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+Route::middleware('auth', 'role:admin')->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+///////////////////////////////////////////////////
+// Vendor Routes//
+///////////////////////////////////////////////////
+
+Route::middleware('auth', 'role:vendor')->group(function () {
+    Route::get('vendor/dashboard', [VendorController::class, 'index'])->name('vendor.dashboard');
+});
+Route::middleware('auth', 'role:vendor')->group(function () {
+    Route::get('vendor/dashboard', [VendorController::class, 'index'])->name('vendor.dashboard');
+});
+
+
+
+
+
+
+
+
+
+require __DIR__ . '/auth.php';
