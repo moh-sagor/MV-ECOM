afe1f02532aa85d08de2af54b2d1a04b5134306c
Sagor Hossain <sagorhassain4@gmail.com>
2023-05-25 00:41:21 +0600

    profile dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <div class="page-header breadcrumb-wrap">
     <div class="container">
         <div class="breadcrumb">
-            <a href="{{route('dashboard')}}" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
+            <a href="{{url('/')}}" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
             <span></span> My Account
         </div>
     </div>
